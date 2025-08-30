7018ab7a77512832613c012365daa0d53936a916
Phlorion <gigadroll44thebest@gmail.com>
2025-08-30 17:27:03 +0300

    Comments

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import "./App.css";
 import MoviesList from "./components/MoviesList";
 import { Routes, Route, Navigate } from "react-router-dom";
 import MovieDetails from "./components/MovieDetails";
+import NavBar from "./components/NavBar";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
+      <nav>
+        <NavBar />
+      </nav>
       <main>
         <Routes>
           <Route path="/" element={<Navigate to={"/movies"} />} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="nav__container">
+      <p className="prevent-select">MoviesLib</p>
+      <ul className="nav__navigation">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/"}>Library</Link>
+        </li>
+        <li>
+          <div className="nav__search_wrapper">
+            <input className="nav__search" placeholder="Search..."></input>
+            <button className="nav__search_btn">
+              <i className="bi bi-search"></i>
+            </button>
+          </div>
+        </li>
+        <li>
+          <Link to={"/"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/"}>Help</Link>
+        </li>
+      </ul>
+      <div className="nav__account_container"></div>
+    </div>
+  );
+};
+
+export default NavBar;
